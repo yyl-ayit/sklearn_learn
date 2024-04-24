7e5f06780768e6ff7e2423abb3fa7b8173153bc2
yang.sir <577624834@qq.com>
2024-04-24 16:22:30 +0800

    day1

--- README.md
+++ README.md
@@ -48,3 +48,27 @@ fig.show()
 ```
 ![LinearRegression](./img/img2.png)
 
+2. 岭回归
+相对于普通最小二乘法， 可以防止过拟合，更健康
+```python
+import numpy as np
+from sklearn import linear_model
+import plotly.graph_objects as go
+
+if __name__ == '__main__':
+    # 随机产生实验数据
+    data_x = [[np.random.randint(0, 1000, size=1)[0]] for i in range(1000)]
+    data_y = np.random.randint(0, 1000, size=1000)
+
+    # 更改参数，查看效果
+    reg = linear_model.Ridge(alpha=0.5)
+    reg.fit(data_x, data_y)
+    x = [i[0] for i in data_x]
+    print(reg.coef_)
+    fig = go.Figure(data=[
+        go.Scatter(x=x, y=data_y, mode='markers', name='原点'),
+        go.Scatter(x=x, y=x * reg.coef_ + reg.intercept_, mode='lines', name='岭回归')
+    ])
+    fig.show()
+```
+![LinearRegression](./img/img3.png)
--- sklearn_learn.py
+++ sklearn_learn.py
@@ -1,33 +1,19 @@
 import numpy as np
-from sklearn.linear_model import LinearRegression
+from sklearn import linear_model
 import plotly.graph_objects as go
-import plotly.io as pio
-import plotly
-# 创建线性回归模型
-reg = LinearRegression()
 
-# 准备数据
-data_x = np.array([[3], [4], [8]])
-data_y = np.array([3, 5, 7])
+if __name__ == '__main__':
+    # 随机产生实验数据
+    data_x = [[np.random.randint(0, 1000, size=1)[0]] for i in range(1000)]
+    data_y = np.random.randint(0, 1000, size=1000)
 
-# 拟合模型
-reg.fit(data_x, data_y)
-
-# 生成x的值
-x = np.array([sum(i) for i in data_x])
-
-# 预测y的值
-y_pred = x * reg.coef_ + reg.intercept_
-
-# 创建图表
-fig = go.Figure(data=[
-    go.Scatter(x=x, y=y_pred, mode='lines', name='线性回归'),
-    go.Scatter(x=x, y=data_y, mode='markers', name='原点')
-])
-
-# fig.write_html("gdp_per_capita.html")
-# 导出图表为图像
-fig.write_html("output.html", full_html=False, include_plotlyjs='cdn')
-# path = './img/linear_regression_chart.png'
-# pio.write_image(fig, path, scale=5, width=800, height=800)
-# pio.write_image(fig, path, format='png', scale=2)
+    # 更改参数，查看效果
+    reg = linear_model.Ridge(alpha=0.5)
+    reg.fit(data_x, data_y)
+    x = [i[0] for i in data_x]
+    print(reg.coef_)
+    fig = go.Figure(data=[
+        go.Scatter(x=x, y=data_y, mode='markers', name='原点'),
+        go.Scatter(x=x, y=x * reg.coef_ + reg.intercept_, mode='lines', name='岭回归')
+    ])
+    fig.show()
