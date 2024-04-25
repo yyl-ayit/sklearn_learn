509de7010e8536bfe716b997aed63d0d69b7ba39
yang.sir <577624834@qq.com>
2024-04-25 18:05:06 +0800

    day2

--- README.md
+++ README.md
@@ -249,3 +249,39 @@ if __name__ == '__main__':
 ```
 运行结果：
 ![支持向量机](./img/img9.png)
+
+## KNeighborsClassifier 例子
+
+```python
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+
+if __name__ == '__main__':
+    breast_cancer = load_breast_cancer()
+    # 特征数据
+    data_x = breast_cancer.data
+    # 对应标签
+    data_y = breast_cancer.target
+    X_train, X_test, y_train, y_test = train_test_split(data_x, data_y, test_size=0.3, random_state=42)
+    transfer = StandardScaler()
+    x_train = transfer.fit_transform(X_train)
+    x_test = transfer.transform(X_test)
+    estimator = KNeighborsClassifier()
+    param_grid = {'n_neighbors': [i for i in range(1, 21)]}
+
+    estimator = GridSearchCV(estimator=estimator, param_grid=param_grid, cv=5)
+    estimator.fit(x_train, y_train)
+    print('estimator.best_score_---', estimator.best_score_)
+    print('estimator.best_estimator_---', estimator.best_estimator_)
+    print('estimator.best_params_---', estimator.best_params_)
+
+    myret = pd.DataFrame(estimator.cv_results_)
+    myret.to_csv(path_or_buf='./mygridsearchcv.csv')
+
+```
+运行结果：
+
+![KNeighborsClassifier](./img/img10.png)
--- sklearn_learn.py
+++ sklearn_learn.py
@@ -1,25 +1,27 @@
-import numpy as np
-from sklearn import datasets
-from sklearn.svm import SVC
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, accuracy_score
-import plotly.graph_objects as go
+
 if __name__ == '__main__':
-    iris = datasets.load_iris()
-    x, y = iris.data, iris.target
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+    breast_cancer = load_breast_cancer()
+    # 特征数据
+    data_x = breast_cancer.data
+    # 对应标签
+    data_y = breast_cancer.target
+    X_train, X_test, y_train, y_test = train_test_split(data_x, data_y, test_size=0.3, random_state=42)
+    transfer = StandardScaler()
+    x_train = transfer.fit_transform(X_train)
+    x_test = transfer.transform(X_test)
+    estimator = KNeighborsClassifier()
+    param_grid = {'n_neighbors': [i for i in range(1, 21)]}
 
-    scaler = StandardScaler()
-    x_train = scaler.fit_transform(X_train)
-    x_test = scaler.transform(X_test)
+    estimator = GridSearchCV(estimator=estimator, param_grid=param_grid, cv=5)
+    estimator.fit(x_train, y_train)
+    print('estimator.best_score_---', estimator.best_score_)
+    print('estimator.best_estimator_---', estimator.best_estimator_)
+    print('estimator.best_params_---', estimator.best_params_)
 
-    clf = SVC(kernel='linear')
-    clf.fit(x_train, y_train)
-
-    y_pred = clf.predict(x_test)
-
-    print(classification_report(y_test, y_pred))
-    print(accuracy_score(y_test, y_pred))
-
-
+    myret = pd.DataFrame(estimator.cv_results_)
+    myret.to_csv(path_or_buf='./mygridsearchcv.csv')
